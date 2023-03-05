0752c5186cfa66eece309c3caad736c92ea1508d
LosFarmosCTL <80157503+LosFarmosCTL@users.noreply.github.com>
2023-03-05 19:04:57 +0100

    feat: add paints support (#2)

---
+++ src/providers/seventv/paints/LinearGradientPaint.hpp
@@ -0,0 +1,32 @@
+#pragma once
+
+#include <utility>
+#include "Paint.hpp"
+
+#include <optional>
+
+namespace chatterino {
+
+class LinearGradientPaint : public Paint
+{
+public:
+    LinearGradientPaint(const QString name, const std::optional<QColor> color,
+                        const QGradientStops stops, const bool repeat,
+                        const float angle, const std::vector<PaintDropShadow>);
+
+    QBrush asBrush(const QColor userColor,
+                   const QRectF drawingRect) const override;
+    std::vector<PaintDropShadow> getDropShadows() const override;
+    bool animated() const override;
+
+private:
+    const QString name_;
+    const std::optional<QColor> color_;
+    const QGradientStops stops_;
+    const bool repeat_;
+    const float angle_;
+
+    const std::vector<PaintDropShadow> dropShadows_;
+};
+
+}  // namespace chatterino
